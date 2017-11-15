5c57b881400b1841882f690ec2575c3bdf29c984
Tugay Demirel <tgydemirel@gmail.com>
2017-11-15 11:17:14 +0300

    Create template

---
+++ src/exchange/app/ExchangeApp.java
@@ -0,0 +1,16 @@
+
+package exchange.app;
+
+/**
+ * @author Java_sabah
+ */
+public class ExchangeApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
